99543ac2a1655a26726a9f74b264b135669223ff
Wellington Vieira dos Santos <wellington.vsantos@gmail.com>
2026-01-30 08:21:44 -0300

    modelos api app aula

---
+++ src/app/services/vendas.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Vendas {
+  private url = "http://10.91.47.129/modelo-api";
+
+  constructor(private http: HttpClient){}
+  
+  // saída para o primeiro end-point
+  operacao(dados:any){
+    return this.http.post(this.url + '/api.php', dados);
+  }
+
+
+}
